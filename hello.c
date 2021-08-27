244b269d2b305f3c3266d305d7e67614ad7d3399
Kareem Keshk <kkeshk2@huskers.unl.edu>
2021-08-27 00:19:06 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Kareem Keshk
+ *         kkeshk2@huskers.unl.edu
+ * Date:   2021/08/20
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kareem Keshk!\n");
+
+  return 0;
+}
